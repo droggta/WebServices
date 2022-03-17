c8da1a3ba30c3623d0308cbc33a2aa45864250e2
droggta <pascalveit2001@gmail.com>
2022-03-18 00:08:18 +0100

    Datenbankmodel für Nutzer eingebaut

---
+++ webservices/src/main/java/Database/NutzerResource.java
@@ -0,0 +1,19 @@
+package Database;
+
+import Test.Fruit;
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Uni;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.List;
+
+@Path("/user")
+@ApplicationScoped
+public class NutzerResource {
+    @GET
+    public Uni<List<Nutzer>> get() {
+        return Nutzer.listAll(Sort.by("vorname"));
+    }
+}
