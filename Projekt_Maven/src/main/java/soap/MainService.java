2e4ca1de7e9c10ca93d72b0b1870744795061492
droggta <pascalveit2001@gmail.com>
2022-03-30 16:20:16 +0200

    mainservice soap test

--- Projekt_Maven/src/main/java/soap/MainService.java
+++ Projekt_Maven/src/main/java/soap/MainService.java
@@ -1,7 +1,17 @@
 package soap;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.bind.annotation.XmlElement;
 
 @WebService
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL,
+parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public interface MainService {
+    @WebMethod
+    @WebResult(name = "mainService")
+    String test(@WebParam(name = "testData")  @XmlElement(required = true) String testData);
 }
--- Projekt_Maven/src/main/java/soap/MainServiceImpl.java
+++ Projekt_Maven/src/main/java/soap/MainServiceImpl.java
@@ -1,4 +1,11 @@
 package soap;
 
-public class MainServiceImpl {
+import javax.jws.WebService;
+
+@WebService(name = "main", serviceName = "mainService")
+public class MainServiceImpl implements MainService{
+    @Override
+    public String test(final String testData) {
+        return null;
+    }
 }
